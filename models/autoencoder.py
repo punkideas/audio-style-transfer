85b29172dc89590e612657b17c1c796a9be454c1
Anthony Perez <aperez8@stanford.edu>
2017-05-20 09:49:42 +0000

    Autoencoder is working somehow!

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -8,29 +8,34 @@ def seq2seq_ae(input_batch, seq_lengths, is_training, seed=12321):
     training: If true, use mean of batch for batch norm, otherwise using moving average
     """
     original_seq_lengths = seq_lengths
-    p = "valid"
-    net = bn(input_batch, is_training)
-    layer1 = tf.layers.conv1d(net, 256, 11, strides=1, padding=p, use_bias=False,
+    p = "VALID"
+    net = bn(input_batch, is_training, "bn1")
+    layer1 = tf.layers.conv1d(net, 256, 11, strides=1, padding=p, use_bias=False, name="layer1",
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
     seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 11 + 1) / 1.)
-    net = bn(layer1, training=is_training)
-    layer2 = tf.layers.conv1d(net, 256, 5, strides=2, padding=p, use_bias=False,
+    net = bn(layer1, is_training, "bn2")
+    layer2 = tf.layers.conv1d(net, 256, 5, strides=2, padding=p, use_bias=False, name="layer2",
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
     seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 5 + 1) / 2.)
-    net = tf.layers.batch_normalization(layer2, training=is_training)
-    layer3 = tf.layers.conv1d(net, 256, 3, strides=2, padding=p, use_bias=False,
+    net = bn(layer2, is_training, "bn3")
+    layer3 = tf.layers.conv1d(net, 256, 3, strides=2, padding=p, use_bias=False, name="layer3",
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))   
-    net = bn(layer3, training=is_training)
+    net = bn(layer3, is_training, "bn4")
     seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 3 + 1) / 2.)
+
     encoder_outputs, encoder_final_state_tuple = lstm_layer(net, 512, seq_lengths)
-    decoder_outputs = self_feeding_lstm_layer(tf.cast(tf.reduce_max(seq_lengths), tf.int32), encoder_final_state_tuple.c, encoder_final_state_tuple.h)
-    net = conv1d_transpose(decoder_outputs, tf.get_shape(layer2), 256, window_size=3, stride=2, padding=p)
-    net = bn(net, training=is_training)
-    net = conv1d_transpose(net, tf.get_shape(layer1), 256, window_size=5, stride=2, padding=p)
-    net = bn(net, training=is_training)
-    net = conv1d_transpose(net, tf.get_shape(input_batch), input_batch.get_shape()[-1], 
-                            window_size=11, stride=1, padding=p)
-    last_layer_bias = tf.get_variable("last_layer_bias", initilizer=tf.zeros((input_batch.get_shape()[-1])))
+    with tf.variable_scope("decoder"):
+        decoder_outputs = self_feeding_lstm_layer(get_tf_shape_as_list(encoder_outputs)[1], encoder_final_state_tuple.c, encoder_final_state_tuple.h)
+
+    # TODO double check that these conv1d_transposes are actually outputting the correct size    
+
+    net = conv1d_transpose(decoder_outputs, get_tf_shape_as_list(layer2), 256, window_size=3, stride=2, padding=p, name="layer_d1")
+    net = bn(net, is_training, "bn_d1")
+    net = conv1d_transpose(net, get_tf_shape_as_list(layer1), 256, window_size=5, stride=2, padding=p, name="layer_d2")
+    net = bn(net, is_training, "bn_d2")
+    net = conv1d_transpose(net, get_tf_shape_as_list(input_batch), input_batch.get_shape()[-1], 
+                            window_size=11, stride=1, padding=p, name="layer_d3")
+    last_layer_bias = tf.get_variable("last_layer_bias", initializer=tf.zeros((input_batch.get_shape()[-1])))
     net += last_layer_bias
     return net, [layer1, layer2, layer3, encoder_final_state_tuple.c]
     
@@ -42,5 +47,6 @@ def seq2seq_ae_with_loss(input_batch, seq_lengths, training, seed=12321):
     """
     ae_output, layer_features = seq2seq_ae(input_batch, seq_lengths, training, seed=seed)
     mask = tf.sequence_mask(seq_lengths, maxlen=input_batch.get_shape()[1]) # Fine if None, will take max length
-    loss = tf.nn.l2_loss((input_batch - ae_output) * mask)
+    mask = tf.expand_dims(tf.cast(mask, tf.float32), axis=2)
+    loss = tf.nn.l2_loss((input_batch - ae_output) * mask) / tf.cast(get_tf_shape_as_list(input_batch)[0], tf.float32)
     return ae_output, layer_features, loss
--- models/layers.py
+++ models/layers.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
 
-def get_tf_shape_as_list(x):
+def get_tf_shape_as_list(x, mix_python=True):
     tf_shape = tf.shape(x)
     tensor_shape_vals = tf.split(tf_shape, tf_shape.get_shape().as_list()[0])
-    return [tf.squeeze(x) for x in tensor_shape_vals]
+    tf_shape_list = [tf.squeeze(_x) for _x in tensor_shape_vals]
+    out = []
+    for i, python_shape_val in enumerate(x.get_shape().as_list()):
+        if mix_python and python_shape_val is not None:
+            out.append(python_shape_val)
+        else:
+            out.append(tf_shape_list[i])
+    return out
 
 def lstm_layer(x, hidden_size, seq_lengths, initial_state=None, seed=123123):
     cell = tf.contrib.rnn.LSTMCell(hidden_size, initializer= \
@@ -17,7 +24,6 @@ def self_feeding_lstm_layer(max_output_sequence_length, initial_c_state, initial
     An LSTM that will use the previous output as the current input
     """
     tf_batch_size, hidden_size = get_tf_shape_as_list(initial_c_state)
-    _, hidden_size = initial_c_state.get_shape().as_list()
     cell = tf.contrib.rnn.LSTMCell(hidden_size, initializer= \
                                    tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
     index = tf.constant(0)
@@ -30,26 +36,30 @@ def self_feeding_lstm_layer(max_output_sequence_length, initial_c_state, initial
     def body(i, outs, next_i, next_c_m):
         new_output, new_state = cell(next_i, next_c_m)
         return i + 1, tf.concat((outs, tf.expand_dims(new_output, axis=0)), axis=0), new_output, (new_state.c, new_state.h) 
+    shape_invariants=[index.get_shape(), tf.TensorShape([None, None, hidden_size]),
+                      next_input.get_shape(), (initial_c_state.get_shape(), initial_m_state.get_shape())]
     _, outputs, _, _ = tf.while_loop(cond, body, [index, outputs, next_input, next_state],
-                  shape_invariants=[index.get_shape(), tf.TensorShape([None, tf_batch_size, hidden_size]),
-                                    next_input.get_shape(), (initial_c_state.get_shape(), initial_m_state.get_shape())],
-                  parallel_iterations=1)
+                  shape_invariants=shape_invariants, parallel_iterations=1)
     return tf.transpose(outputs, (1, 0, 2))
     
-def conv1d_transpose(x, output_shape, filters_out, window_size, stride, padding, seed=123123):
+def conv1d_transpose(x, output_shape, filters_out, window_size, stride, padding, name=None, seed=123123):
     """
+    output_shape must be a python list, and the last dimension must be known
     No bias, 1d version of tf.nn.conv2d_transpose
     """
-    filter = tf.get_variable("conv1d_filter", shape=(1, window_size, filters_out, x.get_shape()[-1]),
-                            dtype=tf.float32, 
-                            initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
-    return tf.nn.conv2d_transpose(
-    x, filter, output_shape=output_shape,
-    strides = (1, 1, stride, 1),
-    padding=padding)
+    with tf.variable_scope(name):
+	x = tf.expand_dims(x, axis=1)
+	B, W, C = output_shape
+	output_shape = tf.stack((B, 1, W, C))
+	filter = tf.get_variable("conv1d_filter", shape=(1, window_size, filters_out, x.get_shape()[-1]),
+				dtype=tf.float32, 
+				initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
+	x = tf.nn.conv2d_transpose(x, filter, output_shape=output_shape,
+	strides = (1, 1, stride, 1), padding=padding)
+	return tf.squeeze(x, axis=[1])
     
 
-def bn(x, training):
+def bn(x, training, name=None):
     return tf.layers.batch_normalization(
 	x,
 	axis=-1,
@@ -65,6 +75,6 @@ def bn(x, training):
 	gamma_regularizer=None,
 	training=training,
 	trainable=True,
-	name=None,
+	name=name,
 	reuse=None)    
 	
