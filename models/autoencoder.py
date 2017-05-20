89b7301cc55c1eb595a803810a129b078ebb68d6
Anthony Perez <aperez8@stanford.edu>
2017-05-20 08:44:51 +0000

    Debugging autoencoder style transfer... checkpoint

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from layers import *
 
-def seq2seq_ae(input_batch, seq_lengths, training, seed=12321):
+def seq2seq_ae(input_batch, seq_lengths, is_training, seed=12321):
     """
     input_batch: The (B, T, F) shaped tensor representing audio.  B - batch size, T - time dimension, F - features
     seq_lengths: (b,) shape tensor containing the sequence masks
@@ -9,30 +9,30 @@ def seq2seq_ae(input_batch, seq_lengths, training, seed=12321):
     """
     original_seq_lengths = seq_lengths
     p = "valid"
-    net = batch_normalization(input_batch, training=training)
+    net = bn(input_batch, is_training)
     layer1 = tf.layers.conv1d(net, 256, 11, strides=1, padding=p, use_bias=False,
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
-    seq_lengths = tf.ceil((seq_lengths - 11 + 1) / 1.)
-    net = batch_normalization(layer1, training=training)
+    seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 11 + 1) / 1.)
+    net = bn(layer1, training=is_training)
     layer2 = tf.layers.conv1d(net, 256, 5, strides=2, padding=p, use_bias=False,
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
-    seq_lengths = tf.ceil((seq_lengths - 5 + 1) / 2.)
-    net = batch_normalization(layer2, training=training)
+    seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 5 + 1) / 2.)
+    net = tf.layers.batch_normalization(layer2, training=is_training)
     layer3 = tf.layers.conv1d(net, 256, 3, strides=2, padding=p, use_bias=False,
                      kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))   
-    net = batch_normalization(layer3, training=training)
-    seq_lengths = tf.ceil((seq_lengths - 3 + 1) / 2.)
-    encoder_outputs, encoder_final_state = lstm_layer(net, 512, seq_lengths)
-    decoder_outputs = self_feeding_lstm_layer(512, tf.reduce_max(seq_lengths), encoder_final_state)
+    net = bn(layer3, training=is_training)
+    seq_lengths = tf.ceil((tf.cast(seq_lengths, tf.float32) - 3 + 1) / 2.)
+    encoder_outputs, encoder_final_state_tuple = lstm_layer(net, 512, seq_lengths)
+    decoder_outputs = self_feeding_lstm_layer(tf.cast(tf.reduce_max(seq_lengths), tf.int32), encoder_final_state_tuple.c, encoder_final_state_tuple.h)
     net = conv1d_transpose(decoder_outputs, tf.get_shape(layer2), 256, window_size=3, stride=2, padding=p)
-    net = batch_normalization(net, training=training)
+    net = bn(net, training=is_training)
     net = conv1d_transpose(net, tf.get_shape(layer1), 256, window_size=5, stride=2, padding=p)
-    net = batch_normalization(net, training=training)
+    net = bn(net, training=is_training)
     net = conv1d_transpose(net, tf.get_shape(input_batch), input_batch.get_shape()[-1], 
                             window_size=11, stride=1, padding=p)
     last_layer_bias = tf.get_variable("last_layer_bias", initilizer=tf.zeros((input_batch.get_shape()[-1])))
     net += last_layer_bias
-    return net, [layer1, layer2, layer3, encoder_final_state]
+    return net, [layer1, layer2, layer3, encoder_final_state_tuple.c]
     
 def seq2seq_ae_with_loss(input_batch, seq_lengths, training, seed=12321):
     """
--- models/layers.py
+++ models/layers.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 
 def get_tf_shape_as_list(x):
     tf_shape = tf.shape(x)
-    return tf.split(tf_shape, tf_shape.get_shape()[0])
+    tensor_shape_vals = tf.split(tf_shape, tf_shape.get_shape().as_list()[0])
+    return [tf.squeeze(x) for x in tensor_shape_vals]
 
 def lstm_layer(x, hidden_size, seq_lengths, initial_state=None, seed=123123):
     cell = tf.contrib.rnn.LSTMCell(hidden_size, initializer= \
@@ -11,24 +12,28 @@ def lstm_layer(x, hidden_size, seq_lengths, initial_state=None, seed=123123):
                       initial_state=initial_state, dtype=tf.float32)
     return outputs, final_state
     
-def self_feeding_lstm_layer(hidden_size, max_output_sequence_length, initial_state, seed=123123):
+def self_feeding_lstm_layer(max_output_sequence_length, initial_c_state, initial_m_state, seed=123123):
     """
     An LSTM that will use the previous output as the current input
     """
+    tf_batch_size, hidden_size = get_tf_shape_as_list(initial_c_state)
+    _, hidden_size = initial_c_state.get_shape().as_list()
     cell = tf.contrib.rnn.LSTMCell(hidden_size, initializer= \
                                    tf.contrib.layers.xavier_initializer(uniform=False, seed=seed))
-    tf_batch_size = get_tf_shape_as_list(initial_state)[0]
     index = tf.constant(0)
     outputs = tf.zeros((0, tf_batch_size, hidden_size))
-    next_input = tf.get_variable("self_feeding_lstm_initial_input", initializer=tf.zeros((hidden_size,)))
-    next_state = initial_state
-    def cond(i, outs, next_i, next_c):
+    next_input = tf.get_variable("self_feeding_lstm_initial_input", initializer=tf.zeros((1, hidden_size,)))
+    next_input = tf.tile(next_input, (tf_batch_size, 1))
+    next_state = (initial_c_state, initial_m_state)
+    def cond(i, outs, next_i, next_c_m):
         return tf.less(i, max_output_sequence_length)
-    def body(i, outs, next_i, next_c):
-        new_output, new_state = cell(next_i, next_c)
-        return i + 1, tf.concat((os, new_output), axis=0), new_output, new_state 
+    def body(i, outs, next_i, next_c_m):
+        new_output, new_state = cell(next_i, next_c_m)
+        return i + 1, tf.concat((outs, tf.expand_dims(new_output, axis=0)), axis=0), new_output, (new_state.c, new_state.h) 
     _, outputs, _, _ = tf.while_loop(cond, body, [index, outputs, next_input, next_state],
-                  shape_invariants=None, parallel_iterations=1)
+                  shape_invariants=[index.get_shape(), tf.TensorShape([None, tf_batch_size, hidden_size]),
+                                    next_input.get_shape(), (initial_c_state.get_shape(), initial_m_state.get_shape())],
+                  parallel_iterations=1)
     return tf.transpose(outputs, (1, 0, 2))
     
 def conv1d_transpose(x, output_shape, filters_out, window_size, stride, padding, seed=123123):
@@ -42,5 +47,24 @@ def conv1d_transpose(x, output_shape, filters_out, window_size, stride, padding,
     x, filter, output_shape=output_shape,
     strides = (1, 1, stride, 1),
     padding=padding)
-        
     
+
+def bn(x, training):
+    return tf.layers.batch_normalization(
+	x,
+	axis=-1,
+	momentum=0.99,
+	epsilon=0.001,
+	center=True,
+	scale=True,
+	beta_initializer=tf.zeros_initializer(),
+	gamma_initializer=tf.ones_initializer(),
+	moving_mean_initializer=tf.zeros_initializer(),
+	moving_variance_initializer=tf.ones_initializer(),
+	beta_regularizer=None,
+	gamma_regularizer=None,
+	training=training,
+	trainable=True,
+	name=None,
+	reuse=None)    
+	
