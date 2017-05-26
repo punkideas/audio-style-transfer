6ba64b77481ed2a20b08e53059bf5be113134641
Anthony Perez <aperez8@stanford.edu>
2017-05-26 23:08:23 +0000

    Changed the name of some paramters in main,  also the last few commits were for adding a fixed length version of the auto encoder

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ flags.DEFINE_string('experiment_name', 'first_run', 'Name of the experiment bein
 flags.DEFINE_string('data_dir', None, "directory where data is stored")
 flags.DEFINE_string('checkpoint_dir', './saved_checkpoints/', 'Checkpoint directory')
 flags.DEFINE_integer('batch_size', 100, "The batch size")
-flags.DEFINE_integer('gan_batch_size', 40, "The batch size")
+flags.DEFINE_integer('fixed_len_batch_size', 40, "The batch size")
 flags.DEFINE_float('learning_rate', 1e-3, "The learning rate")
 flags.DEFINE_float('gpu_usage', 0.96, "The gpu usage as a percentage")
 flags.DEFINE_integer('num_epochs', 20, "The number of epochs to train")
@@ -45,14 +45,14 @@ def main(_):
                 
     suffix = "_conv_ae"
     conv_ae.train_conv_ae(FLAGS.data_dir, FLAGS.experiment_name + suffix, \
-                FLAG_checkpoint_dir + suffix, FLAG_log_dir + suffix, FLAGS.batch_size, \
+                FLAG_checkpoint_dir + suffix, FLAG_log_dir + suffix, FLAGS.fixed_len_batch_size, \
                 FLAGS.learning_rate, FLAGS.num_epochs, FLAGS.gpu_usage, \
                 FLAGS.tag, FLAG_best_model_tag)
      
     if FLAGS.gan:
         suffix = "_gan"
         gan.train_gan(FLAGS.data_dir, FLAGS.experiment_name + suffix, \
-                    FLAG_checkpoint_dir + suffix, FLAG_log_dir + suffix, FLAGS.batch_size, \
+                    FLAG_checkpoint_dir + suffix, FLAG_log_dir + suffix, FLAGS.fixed_len_batch_size, \
                     FLAGS.learning_rate, FLAGS.num_epochs, FLAGS.gpu_usage, \
                     FLAGS.tag, FLAG_best_model_tag)
 
